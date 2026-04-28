0b26eb033be4bf422442c632174a72daa0a9001f
KTDP\mahusayr <Rogelio.Mahusayjr@ddp.kyocera.com>
2026-04-28 12:42:39 +0800

    PNG files are corrupted. Updated PNG files for temporary. Tried putting a GIF for movement but is not working

--- src/MarioFSM.java
+++ src/MarioFSM.java
@@ -24,18 +24,21 @@ public class MarioFSM extends JPanel implements ActionListener {
     private int SCREEN_HEIGHT;
     private BufferedImage marioJumping;
 
-    enum State {IDLE, MOVING, JUMP_IDLE, JUMP_MOVING, CROUCHING}
+    enum State {
+        IDLE, MOVING, JUMP_IDLE, JUMP_MOVING, CROUCHING
+    }
+
     int[][] table = {
-            {1,1,0,0,2,0,4},
-            {1,1,0,0,3,1,1},
-            {3,3,2,2,2,2,2},
-            {3,3,2,2,3,3,3},
-            {4,4,4,4,4,0,4}
+            { 1, 1, 0, 0, 2, 0, 4 },
+            { 1, 1, 0, 0, 3, 1, 1 },
+            { 3, 3, 2, 2, 2, 2, 2 },
+            { 3, 3, 2, 2, 3, 3, 3 },
+            { 4, 4, 4, 4, 4, 0, 4 }
     };
 
     int state = 0, input = 0;
 
-    private BufferedImage marioSprite;
+    private BufferedImage marioSprite, marioMoving;
     private BufferedImage background;
 
     private State currentState = State.IDLE;
@@ -47,13 +50,13 @@ public class MarioFSM extends JPanel implements ActionListener {
 
     public MarioFSM(int screenWidth, int screenHeight) {
         SCREEN_WIDTH = screenWidth;
-        SCREEN_HEIGHT =  screenHeight;
+        SCREEN_HEIGHT = screenHeight;
         loadImages();
         Timer timer = new Timer(20, this);
         timer.start();
         setFocusable(true);
 
-        addKeyListener(new KeyAdapter(){
+        addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 handleInput(e.getKeyCode(), true);
@@ -66,15 +69,18 @@ public class MarioFSM extends JPanel implements ActionListener {
         });
     }
 
-    private void loadImages(){
+    private void loadImages() {
         try {
             // Loading images from the project folder
-            marioSprite = ImageIO.read(new File("resources/mario_idle.png"));
-            marioJumping = ImageIO.read(new File("resources/mario_jumping.png"));
-//            marioSprite = ImageIO.read(new File("D:\\Projects\\Spring Boot\\Mario - Application of Finite State Machine\\resources\\mario_idle.png"));
-//            marioJumping = ImageIO.read(new File("D:\\Projects\\Spring Boot\\Mario - Application of Finite State Machine\\resources\\mario_jumping.png"));
+            marioSprite = ImageIO.read(new File("../resources/mario_idle.png"));
+            marioJumping = ImageIO.read(new File("../resources/mario_jumping.png"));
+            marioMoving = ImageIO.read(new File("../resources/mario_moving.gif"));
+            // marioSprite = ImageIO.read(new File("D:\\Projects\\Spring Boot\\Mario -
+            // Application of Finite State Machine\\resources\\mario_idle.png"));
+            // marioJumping = ImageIO.read(new File("D:\\Projects\\Spring Boot\\Mario -
+            // Application of Finite State Machine\\resources\\mario_jumping.png"));
 
-//            background = ImageIO.read(new File("resources/background.png"));
+            // background = ImageIO.read(new File("resources/background.png"));
         } catch (IOException e) {
             System.out.println("Error: Could not find image files!");
             e.printStackTrace();
@@ -82,44 +88,45 @@ public class MarioFSM extends JPanel implements ActionListener {
     }
 
     public void handleInput(int keyCode, boolean pressed) {
-//        switch (currentState) {
-//            case IDLE:
-//                if (pressed && keyCode == KeyEvent.VK_RIGHT){
-//                    direction = 1;
-//                    currentState = State.MOVING;
-//                }
-//                if (pressed && keyCode == KeyEvent.VK_LEFT){
-//                    direction = 0;
-//                    currentState = State.MOVING;
-//                }
-//                if (pressed && keyCode == KeyEvent.VK_SPACE) startJump();
-//                if (pressed && keyCode == KeyEvent.VK_DOWN) currentState = State.CROUCHING;
-//                break;
-//
-//            case MOVING:
-//                if (!pressed && keyCode == KeyEvent.VK_RIGHT || keyCode ==  KeyEvent.VK_LEFT) currentState = State.IDLE;
-//                if (pressed && keyCode == KeyEvent.VK_SPACE) startJump();
-//                break;
-//
-//            case JUMPING:
-//                // In a pure DFA, transitions out of JUMPING
-//                // usually happen via physics events, not just key presses.
-//                break;
-//
-//            case CROUCHING:
-//                if (!pressed && keyCode == KeyEvent.VK_DOWN) currentState = State.IDLE;
-//                break;
-//        }
+        // switch (currentState) {
+        // case IDLE:
+        // if (pressed && keyCode == KeyEvent.VK_RIGHT){
+        // direction = 1;
+        // currentState = State.MOVING;
+        // }
+        // if (pressed && keyCode == KeyEvent.VK_LEFT){
+        // direction = 0;
+        // currentState = State.MOVING;
+        // }
+        // if (pressed && keyCode == KeyEvent.VK_SPACE) startJump();
+        // if (pressed && keyCode == KeyEvent.VK_DOWN) currentState = State.CROUCHING;
+        // break;
+        //
+        // case MOVING:
+        // if (!pressed && keyCode == KeyEvent.VK_RIGHT || keyCode == KeyEvent.VK_LEFT)
+        // currentState = State.IDLE;
+        // if (pressed && keyCode == KeyEvent.VK_SPACE) startJump();
+        // break;
+        //
+        // case JUMPING:
+        // // In a pure DFA, transitions out of JUMPING
+        // // usually happen via physics events, not just key presses.
+        // break;
+        //
+        // case CROUCHING:
+        // if (!pressed && keyCode == KeyEvent.VK_DOWN) currentState = State.IDLE;
+        // break;
+        // }
 
-        if(pressed && keyCode == KeyEvent.VK_LEFT){
+        if (pressed && keyCode == KeyEvent.VK_LEFT) {
             input = LEFT;
             direction = LEFT;
             lastFacingDirection = LEFT;
-        } else if(pressed && keyCode == KeyEvent.VK_RIGHT){
+        } else if (pressed && keyCode == KeyEvent.VK_RIGHT) {
             input = RIGHT;
             direction = RIGHT;
             lastFacingDirection = RIGHT;
-        } else if (!pressed && keyCode == KeyEvent.VK_LEFT){
+        } else if (!pressed && keyCode == KeyEvent.VK_LEFT) {
             input = RELEASE_LEFT;
             direction = RELEASE_LEFT;
         } else if (!pressed && keyCode == KeyEvent.VK_RIGHT) {
@@ -130,7 +137,7 @@ public class MarioFSM extends JPanel implements ActionListener {
             startJump();
         } else if (!pressed && keyCode == KeyEvent.VK_DOWN) {
             input = RELEASE_DOWN;
-        } else if (pressed && keyCode == KeyEvent.VK_DOWN){
+        } else if (pressed && keyCode == KeyEvent.VK_DOWN) {
             input = DOWN;
         }
 
@@ -141,12 +148,12 @@ public class MarioFSM extends JPanel implements ActionListener {
         System.out.println(currentState);
     }
 
-    private void printKey(int keyCode, boolean pressed){
-        if(pressed && keyCode == KeyEvent.VK_LEFT){
+    private void printKey(int keyCode, boolean pressed) {
+        if (pressed && keyCode == KeyEvent.VK_LEFT) {
             System.out.println("VK_LEFT");
-        } else if(pressed && keyCode == KeyEvent.VK_RIGHT){
+        } else if (pressed && keyCode == KeyEvent.VK_RIGHT) {
             System.out.println("VK_RIGHT");
-        } else if (!pressed && keyCode == KeyEvent.VK_LEFT){
+        } else if (!pressed && keyCode == KeyEvent.VK_LEFT) {
             System.out.println("VK_RELEASE_LEFT");
         } else if (!pressed && keyCode == KeyEvent.VK_RIGHT) {
             System.out.println("VK_RELEASE_RIGHT");
@@ -154,38 +161,38 @@ public class MarioFSM extends JPanel implements ActionListener {
             System.out.println("VK_SPACE");
         } else if (!pressed && keyCode == KeyEvent.VK_DOWN) {
             System.out.println("VK_RELEASE_DOWN");
-        } else if (pressed && keyCode == KeyEvent.VK_DOWN){
+        } else if (pressed && keyCode == KeyEvent.VK_DOWN) {
             System.out.println("VK_DOWN");
         }
     }
 
     private void startJump() {
-        if(currentState != State.JUMP_IDLE && currentState != State.JUMP_MOVING)
+        if (currentState != State.JUMP_IDLE && currentState != State.JUMP_MOVING)
             velocityY = -15; // Upward force
     }
 
-    private void performJump(){
+    private void performJump() {
         marioY += velocityY;
         velocityY += 1; // Gravity
 
         if (marioY >= GROUND_Y) {
             marioY = GROUND_Y;
-            if(currentState == State.JUMP_IDLE){
+            if (currentState == State.JUMP_IDLE) {
                 currentState = State.IDLE;
                 state = State.IDLE.ordinal();
-            } else if (currentState == State.JUMP_MOVING){
+            } else if (currentState == State.JUMP_MOVING) {
                 currentState = State.MOVING;
                 state = State.MOVING.ordinal();
             }
         }
     }
 
-    private void performMove(){
-        if(direction == LEFT){
-            if(marioX >= 8)
+    private void performMove() {
+        if (direction == LEFT) {
+            if (marioX >= 8)
                 marioX -= 5;
-        } else if(direction == RIGHT){
-            if(marioX <= SCREEN_WIDTH - 55)
+        } else if (direction == RIGHT) {
+            if (marioX <= SCREEN_WIDTH - 55)
                 marioX += 5;
         }
     }
@@ -197,8 +204,8 @@ public class MarioFSM extends JPanel implements ActionListener {
             performJump();
         }
 
-        if (currentState == State.JUMP_MOVING){
-//            performMove();
+        if (currentState == State.JUMP_MOVING) {
+            // performMove();
             performJump();
         }
 
@@ -209,64 +216,68 @@ public class MarioFSM extends JPanel implements ActionListener {
     }
 
     @Override
-    protected void paintComponent(Graphics g){
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, getWidth(), getHeight());
 
-//        if (background != null) {
-//            g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
-//        }
+        // if (background != null) {
+        // g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+        // }
 
         if (marioSprite != null) {
             BufferedImage newMario = marioSprite;
-            if(lastFacingDirection == LEFT && (currentState !=  State.JUMP_IDLE && currentState != State.JUMP_MOVING))
+            if (lastFacingDirection == LEFT && (currentState != State.JUMP_IDLE && currentState != State.JUMP_MOVING))
                 newMario = createFlipped(marioSprite);
-            else if (lastFacingDirection == RIGHT && (currentState !=  State.JUMP_IDLE && currentState != State.JUMP_MOVING))
-                newMario = marioSprite;
-            else if (currentState ==  State.JUMP_IDLE || currentState == State.JUMP_MOVING)
-                newMario = marioJumping;
+            else if (lastFacingDirection == RIGHT
+                    && (currentState != State.JUMP_IDLE && currentState != State.JUMP_MOVING))
+                newMario = marioMoving;
+            else if (currentState == State.JUMP_IDLE || currentState == State.JUMP_MOVING) {
+                if (lastFacingDirection == LEFT)
+                    newMario = createFlipped(marioJumping);
+                else
+                    newMario = marioJumping;
+            }
 
             if (currentState == State.CROUCHING) {
                 // Draw Mario shorter/squashed
                 g.drawImage(newMario, marioX, marioY + 20, 40, 30, null);
             } else {
                 // Draw Mario normally
-                    g.drawImage(newMario, marioX, marioY,40, 50, null);
-//                if(direction == LEFT) {
-//                    g.drawImage(marioSprite, marioX, marioY, -40, 50, null);
-//                } else if (direction == RIGHT)
-//                    g.drawImage(marioSprite, marioX, marioY, 40, 50, null);
+                g.drawImage(newMario, marioX, marioY, 40, 50, null);
+                // if(direction == LEFT) {
+                // g.drawImage(marioSprite, marioX, marioY, -40, 50, null);
+                // } else if (direction == RIGHT)
+                // g.drawImage(marioSprite, marioX, marioY, 40, 50, null);
             }
         } else {
             g.setColor(Color.RED);
             if (currentState == State.CROUCHING) {
                 // Draw Mario shorter/squashed
-//                g.drawImage(newMario, marioX, marioY + 20, 40, 30, null);
+                // g.drawImage(newMario, marioX, marioY + 20, 40, 30, null);
                 g.fillRect(marioX, marioY + 20, 40, 30);
             } else {
                 // Draw Mario normally
                 g.fillRect(marioX, marioY, 40, 50);
-//                if(direction == LEFT) {
-//                    g.drawImage(marioSprite, marioX, marioY, -40, 50, null);
-//                } else if (direction == RIGHT)
-//                    g.drawImage(marioSprite, marioX, marioY, 40, 50, null);
+                // if(direction == LEFT) {
+                // g.drawImage(marioSprite, marioX, marioY, -40, 50, null);
+                // } else if (direction == RIGHT)
+                // g.drawImage(marioSprite, marioX, marioY, 40, 50, null);
             }
         }
 
-//        g.setColor(Color.RED);
-//        if(currentState == State.CROUCHING) {
-//            g.fillRect(marioX, marioY + 20, 40, 30);
-//        } else {
-//            g.fillRect(marioX, marioY, 40, 50);
-//        }
+        // g.setColor(Color.RED);
+        // if(currentState == State.CROUCHING) {
+        // g.fillRect(marioX, marioY + 20, 40, 30);
+        // } else {
+        // g.fillRect(marioX, marioY, 40, 50);
+        // }
 
         g.setColor(Color.BLACK);
         g.drawString("Current State: " + currentState, 20, 20);
     }
 
-    private static BufferedImage createFlipped(BufferedImage image)
-    {
+    private static BufferedImage createFlipped(BufferedImage image) {
         AffineTransform at = new AffineTransform();
         at.concatenate(AffineTransform.getScaleInstance(-1, 1));
         at.concatenate(AffineTransform.getTranslateInstance(-image.getWidth(), 0));
@@ -274,8 +285,7 @@ public class MarioFSM extends JPanel implements ActionListener {
     }
 
     private static BufferedImage createTransformed(
-            BufferedImage image, AffineTransform at)
-    {
+            BufferedImage image, AffineTransform at) {
         BufferedImage newImage = new BufferedImage(
                 image.getWidth(), image.getHeight(),
                 BufferedImage.TYPE_INT_ARGB);
@@ -285,6 +295,5 @@ public class MarioFSM extends JPanel implements ActionListener {
         g.dispose();
         return newImage;
     }
-
 
 }
